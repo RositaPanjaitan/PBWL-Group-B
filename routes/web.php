05cf6026d187f68700a618ce7d51ea479a587e50
RositaPanjaitan <Rositapanjaitan0101@gmail.com>
2025-01-07 19:37:27 +0700

    Logo kelas dan foto sekelas

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('mahasiswa', function () {
-    // return view('mahasiswa');
-    $nim = [123,124,125];
-    // while
-    $nilai_awal = 0;
+    return view('home');
 });
